1fe350fef28ddf9a37f629a2a1726580c2f12447
pichan321 <pchan@ithaca.edu>
2023-12-08 11:05:13 -0500

    Simpe shell in Golang

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func eval(line *string) {
+
+}
+
+func parseline(line *string) ([]string, bool) {
+	splits := strings.Fields(*line)
+
+	if splits[len(splits)-1] == "&" {
+		return splits, true
+	}
+
+	return splits, false
+}
+
+func main() {
+	for {
+		line := ""
+		fmt.Scanf("%s", &line)
+
+		eval(&line)
+	}
+}
